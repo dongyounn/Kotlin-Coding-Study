38a48983bc5a16134f9c1041b4bee3b2d9102bba
dongyoun_shin <dyshin1027@gmail.com>
2021-02-21 01:36:47 +0900

    문자열 내림차순으로 배치하기 - 복습

---
+++ src/study/programmers/retry/프로그래머스_20210221_6.kt
@@ -0,0 +1,14 @@
+package study.programmers.retry
+
+/*
+* https://programmers.co.kr/learn/courses/30/lessons/12917
+* 문자열 내림차순으로 배치하기
+* 복습
+* */
+
+fun solution202102216(s: String) =
+    String(s.toCharArray().sortedArrayDescending())
+
+fun main(array: Array<String>) {
+    solution202102216("Zbcdefg")
+}
