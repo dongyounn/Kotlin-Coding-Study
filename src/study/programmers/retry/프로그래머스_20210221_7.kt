5de06f6dd7b05a1556bffe0e977363a192991ea6
dongyoun_shin <dyshin1027@gmail.com>
2021-02-21 01:52:46 +0900

    문자열 내림차순으로 배치하기 - 복습

---
+++ src/study/programmers/retry/프로그래머스_20210221_7.kt
@@ -0,0 +1,21 @@
+package study.programmers.retry
+
+/*
+* https://programmers.co.kr/learn/courses/30/lessons/12917
+* 문자열 내림차순으로 배치하기
+* 복습
+* */
+
+fun solution202102217(s: String): Boolean {
+    var result = true
+    return if (s.length == 4 || s.length == 6) {
+        s.toCharArray().forEach {
+            if (!it.isDigit()) result = false
+        }
+        result
+    } else !result
+}
+
+fun main(array: Array<String>) {
+    solution202102217("a234")
+}
