94d2feac136e3a3988575ad42f6cf8234c3ce756
dongyoun_shin <dyshin1027@gmail.com>
2021-02-21 02:10:05 +0900

    수박수박수박수? - 복습

---
+++ src/study/programmers/retry/프로그래머스_20210221_9.kt
@@ -0,0 +1,17 @@
+package study.programmers.retry
+
+import java.lang.StringBuilder
+
+/*
+* https://programmers.co.kr/learn/courses/30/lessons/12922
+* 수박수박수박수?
+* 복습
+* */
+
+fun solution202102219(n: Int): String =
+    StringBuilder("수박".repeat(n.div(2))).append(if (n.rem(2) == 1) "수" else "").toString()
+
+
+fun main(array: Array<String>) {
+    solution202102219(3)
+}
