9dae407435dcade6306a921bce415863a278a2bd
dongyoun_shin <dyshin1027@gmail.com>
2021-02-21 20:27:03 +0900

    정수 내림차순으로 정렬하기 - 복습

---
+++ src/study/programmers/retry/프로그래머스_20210221_16.kt
@@ -0,0 +1,15 @@
+package study.programmers.retry
+/*
+* 정수 내림차순으로 배치하기
+* https://programmers.co.kr/learn/courses/30/lessons/12933
+* 복습
+* */
+
+fun solution2021022116(n: Long): Long {
+
+    return n.toString().toList().sortedDescending().joinToString("").toLong()
+}
+
+fun main(args: Array<String>) {
+    solution2021022116(118372)
+}
