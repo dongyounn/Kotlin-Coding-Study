195dce8ef87b7da6df53ba502c3833beae60d3a5
dongyoun_shin <dyshin1027@gmail.com>
2021-02-23 21:47:00 +0900

    프로그래머스 두문제 풀이

---
+++ src/study/programmers/retry/프로그래머스_20210223.kt
@@ -0,0 +1,14 @@
+package study.programmers.retry
+
+/*
+* 짝수와 홀
+* https://programmers.co.kr/learn/courses/30/lessons/12937
+* 복습
+* */
+
+fun solution20210223(num: Int): String =
+    if (num.rem(2) == 1) "Odd" else "Even"
+
+fun main(args: Array<String>) {
+    solution20210223(3)
+}
---
+++ src/study/programmers/retry/프로그래머스_20210223_1.kt
@@ -0,0 +1,15 @@
+package study.programmers.retry
+
+/*
+* 평균구하기
+* https://programmers.co.kr/learn/courses/30/lessons/12944
+* 복습
+* */
+
+fun solution202102231(arr: IntArray): Double =
+    arr.average()
+
+
+fun main(args: Array<String>) {
+    solution202102231(intArrayOf(5,5))
+}
