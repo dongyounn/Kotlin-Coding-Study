24bf23bd0802fde1eb640e05b6a9c018cefe064b
dongyoun_shin <dyshin1027@gmail.com>
2021-02-23 21:36:06 +0900

    codility_lesson3 마무리

--- src/study/codility/codility_lesson3.kt
+++ src/study/codility/codility_lesson3.kt
@@ -12,11 +12,29 @@ fun solution2(A: IntArray): Int {
     var tempValue = 0
     val sumValue = A.sum()
     val resultArray = ArrayList<Int>()
-    A.forEach{
+    A.forEach {
         tempValue += it
-        if (tempValue != sumValue){
-            resultArray.add(abs(sumValue - (tempValue*2)))
+        if (tempValue != sumValue) {
+            resultArray.add(abs(sumValue - (tempValue * 2)))
         }
     }
     return resultArray.sorted()[0]
+}
+
+fun solution3(A: IntArray): Int {
+    // write your code in Kotlin
+    val sortedArray = A.sortedArray()
+    for (i in 0..sortedArray.size.minus(1)) {
+        if (
+            i != sortedArray.size.minus(1)
+            && sortedArray[i] - sortedArray[i + 1] != -1
+        ) {
+            return sortedArray[i].plus(1)
+        }
+    }
+    return 1
+}
+
+fun main(args: Array<String>) {
+    solution3(intArrayOf(2, 3, 1, 5))
 }
