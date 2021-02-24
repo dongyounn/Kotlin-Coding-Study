bcd70afa96e9e942e1f12a169ece1412cc9ce6db
dongyoun_shin <dyshin1027@gmail.com>
2021-02-24 23:38:14 +0900

    x 만큼 간격이 있는 n개 숫자 문제 풀이

---
+++ src/study/programmers/retry/프로그래머스_20210224.kt
@@ -0,0 +1,15 @@
+package study.programmers.retry
+
+/*
+* x만큼 간격이 있는 n개의 숫자
+* https://programmers.co.kr/learn/courses/30/lessons/12954
+* 복습
+* */
+
+fun solution20210224(x: Int, n: Int): LongArray =
+    (1..n.toLong()).map { x.times(it) }.toLongArray()
+
+
+fun main(args: Array<String>) {
+    solution20210224(2, 5)
+}
