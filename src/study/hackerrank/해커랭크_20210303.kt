4094e2299c84f0129335f91f9cb487c247c44c33
dongyoun_shin <dyshin1027@gmail.com>
2021-03-03 23:53:21 +0900

    해커랭크 20210303

---
+++ src/study/hackerrank/해커랭크_20210303.kt
@@ -0,0 +1,31 @@
+package study.hackerrank
+
+
+/*
+* https://www.hackerrank.com/challenges/bon-appetit/problem?isFullScreen=true
+* 해커랭크
+* */
+fun bonAppetit(bill: Array<Int>, k: Int, b: Int): Unit {
+    b.minus(bill.filter { it != bill[k] }.sum().div(2)).let {
+        println(
+            if (it == 0) {
+                "Bon Appetit"
+            } else
+                it.toString()
+        )
+    }
+}
+
+fun main(args: Array<String>) {
+    val nk = readLine()!!.trimEnd().split(" ")
+
+    val n = nk[0].toInt()
+
+    val k = nk[1].toInt()
+
+    val bill = readLine()!!.trimEnd().split(" ").map { it.toInt() }.toTypedArray()
+
+    val b = readLine()!!.trim().toInt()
+
+    println(bonAppetit(bill, k, b))
+}
