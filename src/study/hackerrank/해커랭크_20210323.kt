91dd0c31b2db931aff5d8c3e3610137981a85975
dongyoun_shin <dyshin1027@gmail.com>
2021-03-23 23:41:02 +0900

    해커랭크_20210323 문제풀이

---
+++ src/study/hackerrank/해커랭크_20210323.kt
@@ -0,0 +1,29 @@
+package study.hackerrank
+
+import java.util.*
+
+//https://www.hackerrank.com/challenges/utopian-tree/problem?isFullScreen=true
+
+// Complete the utopianTree function below.
+fun utopianTree(n: Int): Int {
+    return (0..n).fold(
+        0, { j, it ->
+            if (it.rem(2) == 1) j * 2 else j + 1
+        }
+    )
+}
+
+
+fun main(args: Array<String>) {
+    val scan = Scanner(System.`in`)
+
+    val t = scan.nextLine().trim().toInt()
+
+    for (tItr in 1..t) {
+        val n = scan.nextLine().trim().toInt()
+
+        val result = utopianTree(n)
+
+        println(result)
+    }
+}
